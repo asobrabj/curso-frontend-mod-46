78553065d75150748792ee7af23c8c7d03f0367b
asobrabj <j.barbosasilva@yahoo.com.br>
2023-06-17 23:07:10 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './components/Header';
+import CalculatorIMC from './components/IMC';
+import Form from './components/Form/Form'
+import Login from './components/Login/LoginControl'
+
+function App() {
+  return (
+    <div className="App">
+      {/* */}
+      <Header title="Título do APP" />
+      <CalculatorIMC /> 
+
+      {/* */}
+      <Form title="props" /> 
+
+      <Login name={'Marcelo'} />
+
+    </div>
+  );
+}
+
+export default App;
